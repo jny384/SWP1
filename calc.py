b2f6909335a10881e4566d631ecb57a7650bdea5
jny384 <skduddkekffu@naver.com>
2020-06-26 05:47:01 +0900

    issue #4 edit code

---
+++ calc.py
@@ -0,0 +1,18 @@
+from cgi import parse_qs
+from calc_template import html
+
+def application(environ, start_response):
+        d = parse_qs(environ['QUERY_STRING'])
+        a = d.get('a', [''])[0]
+        b = d.get('b', [''])[0]
+        sum, mul = 0, 0
+        if '' not in [a,b]:
+                a, b = int(a), int(b)
+                sum = a + b
+                mul = a * b
+        start_response('200 OK', [
+                ('Content-Type', 'text/html'),
+                ('Content-Length', str(len(response_body)))
+        ])
+        return [response_body]
+
---
+++ calc_template.py
@@ -0,0 +1,14 @@
+html = """
+<html>
+	<body>
+	        <form method="get" action="">
+	                        sum = <input type="number" name="a"> + <input type="number" name="b"> <br><br>
+	                        mul =  <input type="number" name="a"> * <input type="number" name="b">
+                        <input type="submit" value="Submit">
+		</form>
+                sum : %(sum)d <br><br>
+                mul : %(mul)d
+	</body>
+</html>
+"""
+ 
