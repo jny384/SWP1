19328c0dcc7f635da547a255f366913ca0bd3911
jny384 <skduddkekffu@naver.com>
2020-06-26 06:44:42 +0900

    issue #6 input : sum & mul -> a & b

--- calc_template.py
+++ calc_template.py
@@ -2,8 +2,8 @@ html = """
 <html>
 	<body>
 	        <form method="get" action="">
-	                        sum = <input type="number" name="a"> + <input type="number" name="b"> <br><br>
-	                        mul =  <input type="number" name="a"> * <input type="number" name="b">
+	                        a = <input type="number" name="a"> <br><br>
+	                        b = <input type="number" name="b">
                         <input type="submit" value="Submit">
 		</form>
                 sum : %(sum)d <br><br>
