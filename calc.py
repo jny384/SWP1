d110c87031e1449de95e4cd8dd6e04781318e597
jny384 <skduddkekffu@naver.com>
2020-06-26 07:39:40 +0900

    issue #8 try - except

--- calc.py
+++ calc.py
@@ -6,14 +6,14 @@ def application(environ, start_response):
         a = d.get('a', [''])[0]
         b = d.get('b', [''])[0]
         sum, mul = 0, 0
-        if a.isdigit() and b.isdigit():
+	try:
                 a, b = int(a), int(b)
                 sum = a + b
                 mul = a * b
-	else:
+	except ValuError:
 		sum = "None"
 		mul = "None"
-	response_body = html % {'sum':sum, 'mul':mul}
+	response_body = html % {'Sum':sum, 'Mul':mul}
         start_response('200 OK', [
                 ('Content-Type', 'text/html'),
                 ('Content-Length', str(len(response_body)))
